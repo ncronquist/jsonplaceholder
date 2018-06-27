9fe608b1896b8a6300b51031f84b3d44ab1f4da8
Nick Cronquist <nick.cronquist@sap.com>
2018-06-27 09:51:27 -0700

    Adds broken posts example to be debugged

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ const { makeExecutableSchema } = require('graphql-tools');
 const rp = require('request-promise');
 
 const typeDefs = `
-  type Query {
-    hello: String
+    type Query {
+        hello: String
 		users: [User]
-		user(id: Int!): User
-  }
+        user(id: Int!): User
+        posts: [Post]
+    }
+
+    type Post {
+        id: ID,
+        title: String,
+        body: String,
+        author: User
+    }
 
 	type User {
 		id: ID,
@@ -43,6 +51,19 @@ function userResolver(root, args, context) {
     // resolve single user here
 }
 
+function postsResolver(root, args, context) {
+    rp('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+        // console.log('RESPONSE:', response)
+        let posts = JSON.parse(response);
+        return posts;
+    })
+    .catch(error => {
+        console.log('ERROR:', error)
+        throw error;
+    })
+}
+
 // Provide resolver functions for your schema fields
 const resolvers = {
     Query: {
@@ -50,8 +71,9 @@ const resolvers = {
             return 'Hello world!';
         },
         users: usersResolver,
-        user: userResolver
-    },
+        user: userResolver,
+        posts: postsResolver
+    }
 };
 
 // Required: Export the GraphQL.js schema object as "schema"
@@ -59,7 +81,7 @@ const schema = makeExecutableSchema({
     typeDefs,
     resolvers,
 });
-  
+
 
 // Initialize the app
 const app = express();
